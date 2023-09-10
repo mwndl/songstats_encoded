62bbc7b92c4b62525bef9c146d4966eec245b4ae
Marcos Wiendl <127610129+mwndl@users.noreply.github.com>
2023-09-10 19:53:37 -0300

    Update script_lyrics.js

--- public/script_lyrics.js
+++ public/script_lyrics.js
@@ -4,6 +4,7 @@ window.addEventListener('load', () => {
   const searchBtn = document.querySelector('#search-btn');
   const search_input = document.querySelector('#search_input');
 
+  /* DESATIVADO MOMENTANEAMENTE
   const div_country_local = document.querySelector('#div_country_local');
   const country_local_status = document.querySelector('#country_local_status');
   const country_local_text = document.querySelector('#country_local_text');
@@ -12,11 +13,14 @@ window.addEventListener('load', () => {
   const view_lyrics_text = document.querySelector('#view_lyrics_text');
   const view_lyrics_arrow = document.querySelector('#view_lyrics_arrow');
   const lyrics_container = document.querySelector('#lyrics_container');
+  */
+  
   const pusher_container = document.querySelector('#pusher_container');
   const lyrics_pusher = document.querySelector('#lyrics_pusher');
   const close_button_pusher = document.querySelector('#close_button_pusher');
 
   // DOM elements - SPOTIFY
+  /* DESATIVADO APÓS INTEGRAÇÃO COM PLAYER DO SPOTIFY
   const imageInput = document.querySelector('#music_cover');
   const titleInput = document.querySelector('#song_name');
   const trackUrlInput = document.querySelector('#song_name');
@@ -24,21 +28,27 @@ window.addEventListener('load', () => {
   const artistUrlInput = document.querySelector('#artist_name');
   const albumInput = document.querySelector('#album_name');
   const albumUrlInput = document.querySelector('#album_name');
+  */
+  
+  /* DESATIVADO MOMENTANEAMENTE
   const releaseDateInput = document.querySelector('#release_date');
-
+  */
+  
+  const spotifyPreview = document.querySelector('#spotify_iframe_preview');
+  
   const durationInput = document.querySelector('#duration');
   const albumPosition = document.querySelector('#album_position');
   const songPreviewInput = document.querySelector('#song_preview');
   const countriesCounterInput = document.querySelector('#countries_counter');
   const popularityInput = document.querySelector('#popularity');
 
+  /* DESATIVADO APÓS INTEGRAÇÃO COM PLAYER DO SPOTIFY
   const player_button = document.querySelector('#player_button');
   const audio = document.querySelector('#player');
+  */
 
   const trackIdInput = document.querySelector('#track-input');
   const isrcInput = document.querySelector('#isrc-input');
-
-  const spotifyPreview = document.querySelector('#spotify_iframe_preview')
 
   // Get DOM elements - MUSIXMATCH
   const mxm_lyrics_url = document.querySelector('#mxm_lyrics_url');
@@ -57,9 +67,11 @@ window.addEventListener('load', () => {
   const stats_mxm_explicit = document.querySelector('#is_explicit');
   const stats_mxm_instrumental = document.querySelector('#is_instrumental');
 
+  /* DESATIVADO MOMENTANEAMENTE
   const div_lyrics_preview = document.querySelector('#div_lyrics_preview');
   const mxm_lyrics_preview = document.querySelector('#lyrics_preview');
-
+  */
+  
   // Função para salvar o país no navegador
   const saveCountry = (countryCode) => {
     if (countryCode && countryCode.length === 2) {
@@ -72,7 +84,7 @@ window.addEventListener('load', () => {
     }
   };
 
-  const accessToken = '8KuA9GwNbaJYvTD8U6h64beb6d6dd56c';
+  const accessToken = '8KuA9GwNbaJYvTD8U6h64beb6d6dd56c'; // Token 3: Public / Limited
 
   // Function to handle search
   const handleSearch = () => {
@@ -127,19 +139,19 @@ window.addEventListener('load', () => {
       return;
     }
 
-    /* disabled
+    /* DESATIVADO MOMENTANEAMENTE
     country_local_status.className = "status-3 status-gray"
     */
+    
     search_input.value = "";
     // Botão 'X' do container de push
     const close_lyricspusher = () => {
       pusher_container.style = "display:none";
     };
-
     close_button_pusher.addEventListener('click', close_lyricspusher);
     
 
-    // Send request to server.js API for Spotify search
+    // Send a Lyrics request to the internal API
     fetch(`https://songstats-backend3.onrender.com/api/spotify/search/${trackId}?token=${accessToken}`)
       .then((response) => response.json())
       .then((data) => {
@@ -150,8 +162,9 @@ window.addEventListener('load', () => {
         mxmData = data.message.body.musixmatch;
 
         // Spotify data
+
+        /* DESATIVADO APÓS INTEGRAÇÃO COM PLAYER DO SPOTIFY
         const title = spotifyData.track_data.track_name;
-        const spotifyID = spotifyData.track_data.track_id;
         const songURL = `https://open.spotify.com/track/${spotifyID}`;
         const artist = spotifyData.artists_data.artists[0].name;
         const artistID = spotifyData.artists_data.artists[0].artist_id;
@@ -160,22 +173,21 @@ window.addEventListener('load', () => {
         const albumID = spotifyData.album_data.album_id;
         const albumURL = `https://open.spotify.com/album/${albumID}`;
         const image = spotifyData.album_data.images.url;
+        const songPreview = spotifyData.track_data.preview_url;
+
+        */
+        
+        const spotifyID = spotifyData.track_data.track_id;
         const isrc = spotifyData.track_data.isrc;
         const durationMs = spotifyData.track_data.duration_ms;
-
         const durationMinutes = Math.floor(durationMs / 60000);
         const durationSeconds = Math.floor((durationMs % 60000) / 1000);
         const duration = `Length: ${durationMinutes}:${durationSeconds.toString().padStart(2, '0')}`;
-
         const albumPositionN = spotifyData.track_data.disc_position;
         const albumTotalN = spotifyData.album_data.total_tracks;
         const numMarkets = spotifyData.track_data.available_markets.length;
         const popularity = spotifyData.track_data.popularity;
-
         const releaseDate = spotifyData.album_data.release_date.toString().padStart(2, '0');
-
-        const songPreview = spotifyData.track_data.preview_url;
-
         const spot_lyrics = spotifyData.track_data.lyrics_stats.has_lyrics;
         const spot_sync = spotifyData.track_data.lyrics_stats.has_sync;
 
@@ -266,8 +278,9 @@ window.addEventListener('load', () => {
             stats_mxm_instrumental.className = "status-1 status-red";
         }
 
+        /* DESATIVADO MOMENTANEAMENTE!
+        
         // Ativa ou desativa o preview das letras
-
         if (mxm_has_lyrics === 1) {
             div_lyrics_preview.style = ""
             mxm_lyrics_preview.src = mxm_preview
@@ -282,11 +295,18 @@ window.addEventListener('load', () => {
         } else {
             view_lyrics_text.id = "view_lyrics_button";
         }
+        */
 
+        /* DESATIVADO APÓS INTEGRAÇÃO COM PLAYER DO SPOTIFY
+        
         // Collect all artist names and URLs
         const artists = data.artists.map((artist) => {
           return { name: artist.name, url: artist.external_urls.spotify };
         });
+
+        */
+
+        /* DESATIVADO MOMENTANEAMENTE
 
         // Função para verificar se o país está disponível na lista de países da faixa
         const checkCountryAvailability = (countryCode) => {
@@ -306,14 +326,18 @@ window.addEventListener('load', () => {
           }
         };
 
+        */
+
+        /* DESATIVADO APÓS INTEGRAÇÃO COM PLAYER DO SPOTIFY
         // Format artists as HTML anchor tags
         const artistsLinks = artists.map(
           (artist) => `<a href="${artist.url}" target="_blank">${artist.name}</a>`
         );
-
+        */
+        
         // Update DOM elements with Spotify data
 
-        /* Desativado após integração de novo player 
+        /* DESATIVADO APÓS INTEGRAÇÃO COM PLAYER DO SPOTIFY
         
         titleInput.textContent = title;
         trackUrlInput.href = songURL;
@@ -321,31 +345,30 @@ window.addEventListener('load', () => {
         albumInput.textContent = album;
         albumUrlInput.href = albumURL;
         imageInput.style.backgroundImage = `url(${image})`;
+        releaseDateInput.textContent = releaseDate;
+        songPreviewInput.src = songPreview;
+        player_button.className = "play-button"
 
         */
+        
+        spotifyPreview.src = `https://open.spotify.com/embed/track/${spotifyID}?utm_source=generator&theme=0`;
         trackIdInput.value = spotifyID;
         isrcInput.value = isrc;
-        /*
-        releaseDateInput.textContent = releaseDate;
-        */
         durationInput.textContent = duration;
         albumPosition.textContent = `Album Position: ${albumPositionN} of ${albumTotalN}`;
         countriesCounterInput.textContent = `Available in ${numMarkets} markets`;
         popularityInput.textContent = `Spotify Rating: ${popularity}%`;
-        /*
-        songPreviewInput.src = songPreview;
-        */
-        spotifyPreview.src = `https://open.spotify.com/embed/track/${spotifyID}?utm_source=generator&theme=0`;
-        /*
-        player_button.className = "play-button"
-        */
-        
+
+        /* DESATIVADO MOMENTANEAMENTE!
         // Verificar se o país salvo está disponível para a faixa pesquisada
         const selectedCountry = localStorage.getItem('selected_country');
         if (selectedCountry) {
           checkCountryAvailability(selectedCountry);
         }
-        /*
+
+        */
+        
+        /* DESATIVADO APÓS INTEGRAÇÃO COM PLAYER DO SPOTIFY
         if (songPreview) {
           songPreviewInput.src = songPreview;
           audio.volume = 0.5;
@@ -375,10 +398,12 @@ window.addEventListener('load', () => {
       `;
       })
       .catch((error) => {
-        alert(`Error: ${error.message}`);
+        console.error(error);
       });
   };
-    /*
+  
+    /* DESATIVADO MOMENTANEAMENTE!
+    
     function toggleLyrics() {
       if (lyrics_container.style.display === 'none') {
           lyrics_container.style.display = '';
