89e952cf59980beb49daf2e97f3616994eeedbbf
Marcos Wiendl <127610129+mwndl@users.noreply.github.com>
2023-09-29 08:25:16 -0300

    Integrating /id URL

--- public/script_lyrics.js
+++ public/script_lyrics.js
@@ -137,7 +137,9 @@ window.addEventListener('load', () => {
     if (trackUrlRegex.test(inputVal)) {
       const url = new URL(inputVal);
       trackId = url.pathname.split('/').pop();
-
+      // Modify the URL
+      history.pushState({}, "", `/s/${trackId}`);
+      
     } else if (shortSpotifyregex.test(inputVal)) {
       notification1("Shortened links are not yet supported, please provide an 'https://open.spotify.com/track/' link")
       loading_spinner.style = "display:none";
