791bf324ee9f6c53a9c4d47143591641cc59b641
Marcos Wiendl <127610129+mwndl@users.noreply.github.com>
2023-10-23 11:36:18 -0300

    Update script_lyrics.js

--- public/script_lyrics.js
+++ public/script_lyrics.js
@@ -160,6 +160,8 @@ window.addEventListener('load', () => {
 
     const inputVal = search_input.value.trim();
 
+    const query_value = '';
+
     const trackUrlRegex = /^(https?:\/\/open\.spotify\.com\/(?:intl-[a-z]{2}\/)?)?track\/(.+)$/;
     const shortSpotifyregex = /https?:\/\/spotify\.link\/[\w-]+/;
     const studioUrlRegex = /(?:&|\?)player=spotify&(?:.*&)?track_id=([^&\s]+)/;
@@ -222,10 +224,10 @@ window.addEventListener('load', () => {
     } else if (studioUrlRegex.test(inputVal)) {
       const match = inputVal.match(studioUrlRegex);
       if (match) {
-        query_and_value = `spotify_id=${match[1]}`;
+        query_value = `spotify_id=${match[1]}`;
       }
     } else if (idRegex.test(inputVal)) {
-      query_and_value = `spotify_id=${inputVal}`;
+      query_value = `spotify_id=${inputVal}`;
     } else if (inputVal === pushForm) {
       pusher_container.style = "";
       lyrics_pusher.src = `https://musixmatch.typeform.com/to/tFQDvIsp?typeform-s`;
@@ -250,7 +252,7 @@ window.addEventListener('load', () => {
       search_input.value = "";
       return;
     } else if (isrcRegex.test(inputVal)) {
-      query_and_value = `track_isrc=${inputVal}`;
+      query_value = `track_isrc=${inputVal}`;
       loading_spinner.style = "display:none";
       searchBtn.style = "";
       search_input.value = "";
@@ -305,7 +307,7 @@ window.addEventListener('load', () => {
     close_button_pusher.addEventListener('click', close_lyricspusher);
 
     // Send a Lyrics request to the internal API
-    fetch(`https://datamatch-backend.onrender.com/lyricsfinder/search?${query_and_value}&token=${accessToken}&background_mode=${background_mode}&spotify_lyrics=1&mxm_data=1`)
+    fetch(`https://datamatch-backend.onrender.com/lyricsfinder/search?${query_value}&token=${accessToken}&background_mode=${background_mode}&spotify_lyrics=1&mxm_data=1`)
       .then((response) => {
         if (!response.ok) {
           if (response.status === 500) {
