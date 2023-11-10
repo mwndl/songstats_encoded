56f5441a0f26e4aa09e868d2d84c7118a82b0cf1
Marcos Wiendl <127610129+mwndl@users.noreply.github.com>
2023-11-10 17:23:08 -0300

    Add files via upload

--- public/script_home.js
+++ public/script_home.js
@@ -1,1 +1,87 @@
-window.addEventListener("load",(()=>{const e=document.querySelector("#top_tracks_title"),t=document.querySelector("#new_releases_title"),a=document.querySelector("#top_img_div"),r=document.querySelector("#new_img_div"),c=document.querySelector("#top_cover1"),o=document.querySelector("#top_cover2"),n=document.querySelector("#top_cover3"),l=document.querySelector("#new_cover1"),s=document.querySelector("#new_cover2"),d=document.querySelector("#new_cover3");fetch("https://datamatch-backend.onrender.com/charts/thumbs/?token=8KuA9GwNbaJYvTD8U6h64beb6d6dd56c").then((e=>e.json())).then((_=>{const u=_.header.user_country_name;let m,w,v,p,i,y,h,k,b,q,S,g;_.data.top_tracks&&_.data.top_tracks.length>=3&&(m=_.data.top_tracks[2].name,w=_.data.top_tracks[1].name,v=_.data.top_tracks[0].name,p=_.data.top_tracks[2].album_cover,i=_.data.top_tracks[1].album_cover,y=_.data.top_tracks[0].album_cover),_.data.new_releases&&_.data.new_releases.length>=3&&(h=_.data.new_releases[2].name,k=_.data.new_releases[1].name,b=_.data.new_releases[0].name,q=_.data.new_releases[2].album_cover,S=_.data.new_releases[1].album_cover,g=_.data.new_releases[0].album_cover),a.style="",r.style="",e.textContent=`Top Tracks - ${u}`,t.textContent=`New Releases - ${u}`,c.title=m,c.alt=m,c.src=p,o.title=w,o.alt=w,o.src=i,n.title=v,n.alt=v,n.src=y,l.title=h,l.alt=h,l.src=q,s.title=k,s.alt=k,s.src=S,d.title=b,d.alt=b,d.src=g})).catch((e=>{}))}));
+window.addEventListener('load', () => {
+    const top_tracks_title = document.querySelector('#top_tracks_title');
+    const new_releases_title = document.querySelector('#new_releases_title');
+
+    const top_img_div = document.querySelector('#top_img_div');
+    const new_img_div = document.querySelector('#new_img_div');
+
+    const top_cover1 = document.querySelector('#top_cover1');
+    const top_cover2 = document.querySelector('#top_cover2');
+    const top_cover3 = document.querySelector('#top_cover3');
+
+    const new_cover1 = document.querySelector('#new_cover1');
+    const new_cover2 = document.querySelector('#new_cover2');
+    const new_cover3 = document.querySelector('#new_cover3');
+
+    function getThumbs() {
+        const apiUrl = "https://datamatch-backend.onrender.com/charts/thumbs/?token=8KuA9GwNbaJYvTD8U6h64beb6d6dd56c";
+
+        // Fazer a solicitação à API usando fetch
+        fetch(apiUrl)
+            .then(response => response.json())
+            .then(data => {
+
+                const user_country_name = data.header.user_country_name;
+    
+                let topTrackName1, topTrackName2, topTrackName3;
+                let topCover1, topCover2, topCover3;
+                let newTrackName1, newTrackName2, newTrackName3;
+                let newCover1, newCover2, newCover3;
+            
+                if (data.data.top_tracks && data.data.top_tracks.length >= 3) {
+                    topTrackName1 = data.data.top_tracks[2].name;
+                    topTrackName2 = data.data.top_tracks[1].name;
+                    topTrackName3 = data.data.top_tracks[0].name;
+            
+                    topCover1 = data.data.top_tracks[2].album_cover;
+                    topCover2 = data.data.top_tracks[1].album_cover;
+                    topCover3 = data.data.top_tracks[0].album_cover;
+                }
+            
+                if (data.data.new_releases && data.data.new_releases.length >= 3) {
+                    newTrackName1 = data.data.new_releases[2].name;
+                    newTrackName2 = data.data.new_releases[1].name;
+                    newTrackName3 = data.data.new_releases[0].name;
+            
+                    newCover1 = data.data.new_releases[2].album_cover;
+                    newCover2 = data.data.new_releases[1].album_cover;
+                    newCover3 = data.data.new_releases[0].album_cover;
+                }
+
+                top_img_div.style = "";
+                new_img_div.style = "";
+
+                top_tracks_title.textContent = `Top Tracks - ${user_country_name}`;
+                new_releases_title.textContent = `New Releases - ${user_country_name}`;
+                
+                top_cover1.title = topTrackName1;
+                top_cover1.alt = topTrackName1;
+                top_cover1.src = topCover1;
+
+                top_cover2.title = topTrackName2;
+                top_cover2.alt = topTrackName2;
+                top_cover2.src = topCover2;
+
+                top_cover3.title = topTrackName3;
+                top_cover3.alt = topTrackName3;
+                top_cover3.src = topCover3;
+
+                new_cover1.title = newTrackName1;
+                new_cover1.alt = newTrackName1;
+                new_cover1.src = newCover1;
+
+                new_cover2.title = newTrackName2;
+                new_cover2.alt = newTrackName2;
+                new_cover2.src = newCover2;
+
+                new_cover3.title = newTrackName3;
+                new_cover3.alt = newTrackName3;
+                new_cover3.src = newCover3;
+
+            })
+            .catch(error => {
+                console.error("Something went wrong: " + error);
+            });
+    }
+    getThumbs(); 
+});
